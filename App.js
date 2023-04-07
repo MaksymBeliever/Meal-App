7f4fc3de86c68eb1283a86ebda36449c25047401
max <max.beliba@alternatestates.com>
2023-04-07 10:58:18 +0300

    Added some beautiful styles for screens and headers, also implemented dynamically changed header titles

--- App.js
+++ App.js
@@ -10,10 +10,25 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <>
-            <StatusBar style="dark" />
+            <StatusBar style="light" />
             <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: '#351401' },
+                        headerTintColor: '#FFFFFF',
+                        contentStyle: { backgroundColor: '#3f2f25' }
+                    }}
+                >
+                    <Stack.Screen
+                        name="MealsCategories"
+                        component={CategoriesScreen}
+                        options={{
+                            title: 'All Categories',
+                            headerStyle: { backgroundColor: '#351401' },
+                            headerTintColor: '#FFFFFF',
+                            contentStyle: { backgroundColor: '#3f2f25' }
+                        }}
+                    />
                     <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
